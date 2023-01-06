f384495fd20735b852e81db44e67e7cc3286d71b
Schartner <Justin.Schartner@kaldewei.com>
2023-01-06 19:20:50 +0100

    httpserver demo

--- demos/script.js
+++ demos/script.js
@@ -1,1 +1,11 @@
 console.log("Hello, JavaScript!");
+
+const data = {
+    'foo': 69
+}
+
+fetch("./post", {
+    method: "POST",
+    body: JSON.stringify(data)
+}).then(res => res.json())
+    .then(data => console.log(data));
