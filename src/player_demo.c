55fc7a4c831b850fd4520b2bf64d7ac6b3a0a835
Justin <justin.schartner00@gmail.com>
2023-04-16 16:00:40 +0200

    playlist visual

--- src/player_demo.c
+++ src/player_demo.c
@@ -26,6 +26,7 @@
 #define BACKGROUND 0.09412, 0.09412, 0.09412, 1.0
 #define FOREGROUND vec4f(0.451, 0.6392, 0.8 , 1) //0xFFCCA37e
 #define WHITE vec4f(0.8667, 0.8667, 0.8667, 1)
+#define GREY vec4f(0.15, 0.15, 0.15, 1)
 
 #define WIDTH 320
 #define HEIGHT 344
@@ -131,7 +132,7 @@ int main(int argc, char **argv) {
   player_set_volume(&player, 0.2);
 
   float button_width = 24.f;
-  float bar_height = 60.f;
+  float bar_y = 60.f;
   bool drag = false;
   bool was_paused = false;
 
@@ -192,7 +193,7 @@ int main(int argc, char **argv) {
 		
 	}
       }
-    }
+    }    
 	
     gui_get_window_sizef(&gui, &border.x, &border.y);
     bar_width = border.x * 0.8f;
@@ -203,12 +204,89 @@ int main(int argc, char **argv) {
     bar_width = border.x - 2 * bar_margin;
     cursor_base_x = border.x/2.f - bar_width / 2.f;
 
+    bool draw_files = border.x >= 2 * musik_width;
+    float draw_files_height, draw_files_x, draw_files_y;
+    
+    Vec2f pos = vec2f(WIDTH/2 - button_width/2, 4 + button_width*.5f);
+    if(draw_files) {
+      draw_files_height = border.y - bar_y - 2 * bar_margin;
+      draw_files_x = pos.x + musik_width/2 + 2 * button_width;
+      draw_files_y = border.y - draw_files_height;
+    }
+
     renderer.time = (float) acc / 1000.f;
     renderer.resolution = border;
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_BLEND);
     glClearColor(BACKGROUND);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    renderer_set_shader(&renderer, SHADER_FOR_COLOR);
+
+    //FILES_RECT
+    if(draw_files) {
+      renderer_solid_rect(&renderer,
+			  vec2f(draw_files_x, draw_files_y),
+			  vec2f(border.x - draw_files_x - bar_margin, draw_files_height - 1 * bar_margin),
+			  GREY);          
+    }
+    
+    //BAR
+    if(click &&
+       mousex >= cursor_base_x &&
+       mousey >= bar_y &&
+       mousex - cursor_base_x < bar_width &&
+       mousey - bar_y < 8) {	  
+
+      float x = mousex - 8.f;
+      if(x < cursor_base_x) {
+	x = cursor_base_x;
+      }
+      if(x + 16 >= cursor_base_x + bar_width) {
+	x = cursor_base_x + bar_width - 16;
+      }
+
+      float time_ = (x - cursor_base_x) * player.duration_abs / (bar_width - 8.f);
+		
+      player_seek(&player, time_);
+    }
+    renderer_solid_rect(&renderer,
+			vec2f(cursor_base_x, bar_y),
+			vec2f(bar_width, 8),
+			WHITE);
+
+    //CURSOR	
+    float cursor_pos = cursor_base_x;
+
+    if(player.decoder_used) {
+      float timestamp, duration;
+      player_get_timestamp_abs(&player, &timestamp);
+      player_get_duration_abs(&player, &duration);
+
+      if(!drag) {
+	cursor_pos += (bar_width - 8.f) * timestamp / duration;
+	    
+	if(click &&
+	   mousex > cursor_pos &&
+	   mousey > (bar_y - 4) &&
+	   mousex - cursor_pos < 16 &&
+	   mousey - (bar_y - 4) < 16) {
+	  drag = true;
+	  was_paused = !player_stop(&player);
+	}
+      } else {
+	cursor_pos = mousex - 12;
+	if(cursor_pos < cursor_base_x) {
+	  cursor_pos = cursor_base_x;
+	}
+	if(cursor_pos + 16 >= cursor_base_x + bar_width) {
+	  cursor_pos = cursor_base_x + bar_width - 16;
+	}
+      } 
+    }
+
+    renderer_solid_rect(&renderer, vec2f(cursor_pos, bar_y - 4), vec2f(16, 16), FOREGROUND);
+    renderer_flush(&renderer);
 
     renderer.image = font_tex;
     renderer_set_shader(&renderer, SHADER_FOR_TEXT);
@@ -239,11 +317,23 @@ int main(int argc, char **argv) {
       player_get_timestamp(&player, &_time);
     }
 
+    //DRAW_FILES - FILES
+    if(draw_files) {
+      float file_y = draw_files_y + draw_files_height - 1 * bar_margin - (float) font.height;
+      size_t off = 0;
+      while(off < sbuffer.len) {
+	render_line(&renderer, draw_files_x, file_y, &font, sbuffer.data + dir_path_len + off);
+	file_y -= (float) font.height;
+	off += strlen(sbuffer.data + off)+1;
+      }
+
+    }
+
     text = tprintf(&temp, "%.2f", _time);
     float text_width = (float) font_estimate_width2(&font, text);
 
     render_line(&renderer,
-		bar_margin - text_width / 2, bar_height + (float) font.height,
+		bar_margin - text_width / 2, bar_y + (float) font.height,
 		&font, text);
 
     player_get_duration(&player, &_time);
@@ -252,7 +342,7 @@ int main(int argc, char **argv) {
 	
     render_line(&renderer,
 		border.x - bar_margin - .5f * text_width,
-		bar_height + (float) font.height,
+		bar_y + (float) font.height,
 		&font, text);
     renderer_flush(&renderer);
 	
@@ -261,7 +351,6 @@ int main(int argc, char **argv) {
     renderer_set_shader(&renderer, SHADER_FOR_IMAGE);
 
     //PLAY N PAUSE
-    Vec2f pos = vec2f(WIDTH/2 - button_width/2, 4 + button_width*.5f);
     if(click &&
        mousex > pos.x &&
        mousey > pos.y &&
@@ -331,68 +420,10 @@ int main(int argc, char **argv) {
     renderer_set_shader(&renderer, SHADER_FOR_RIPPLE);
     //LOGO - MUSIK
     renderer_image_rect(&renderer,
-			vec2f(pos.x - musik_width / 2, bar_height + 1.5 * button_width),
+			vec2f(pos.x - musik_width / 2, bar_y + 1.5 * button_width),
 			vec2f(musik_width, musik_height), vec2f(0, 0), vec2f(1, 1));
     renderer_flush(&renderer);
 	
-    renderer_set_shader(&renderer, SHADER_FOR_COLOR);
-    //BAR
-    if(click &&
-       mousex >= cursor_base_x &&
-       mousey >= bar_height &&
-       mousex - cursor_base_x < bar_width &&
-       mousey - bar_height < 8) {	  
-
-      float x = mousex - 8.f;
-      if(x < cursor_base_x) {
-	x = cursor_base_x;
-      }
-      if(x + 16 >= cursor_base_x + bar_width) {
-	x = cursor_base_x + bar_width - 16;
-      }
-
-      float time_ = (x - cursor_base_x) * player.duration_abs / (bar_width - 8.f);
-		
-      player_seek(&player, time_);
-    }
-    renderer_solid_rect(&renderer,
-			vec2f(cursor_base_x, bar_height),
-			vec2f(bar_width, 8),
-			WHITE);
-
-    //CURSOR	
-    float cursor_pos = cursor_base_x;
-
-    if(player.decoder_used) {
-      float timestamp, duration;
-      player_get_timestamp_abs(&player, &timestamp);
-      player_get_duration_abs(&player, &duration);
-
-      if(!drag) {
-	cursor_pos += (bar_width - 8.f) * timestamp / duration;
-	    
-	if(click &&
-	   mousex > cursor_pos &&
-	   mousey > (bar_height - 4) &&
-	   mousex - cursor_pos < 16 &&
-	   mousey - (bar_height - 4) < 16) {
-	  drag = true;
-	  was_paused = !player_stop(&player);
-	}
-      } else {
-	cursor_pos = mousex - 12;
-	if(cursor_pos < cursor_base_x) {
-	  cursor_pos = cursor_base_x;
-	}
-	if(cursor_pos + 16 >= cursor_base_x + bar_width) {
-	  cursor_pos = cursor_base_x + bar_width - 16;
-	}
-      } 
-    }
-
-    renderer_solid_rect(&renderer, vec2f(cursor_pos, bar_height - 4), vec2f(16, 16), FOREGROUND);
-    renderer_flush(&renderer);
-
     gui_swap_buffers(&gui);
   }
 
